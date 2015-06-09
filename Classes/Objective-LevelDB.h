8868b8809f972b1a3282d69f036d4072493dd3ba
Quanlong He <kyan.ql.he@gmail.com>
2015-06-09 11:05:47 +0800

    Build as framework

---
+++ Classes/Objective-LevelDB.h
@@ -0,0 +1,19 @@
+//
+//  Objective-LevelDB.h
+//  Objective-LevelDB
+//
+//  Created by Quanlong He on 6/9/15.
+//  Copyright (c) 2015 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Objective-LevelDB.
+FOUNDATION_EXPORT double Objective_LevelDBVersionNumber;
+
+//! Project version string for Objective-LevelDB.
+FOUNDATION_EXPORT const unsigned char Objective_LevelDBVersionString[];
+
+#import "LevelDB.h"
+#import "LDBSnapshot.h"
+#import "LDBWriteBatch.h
