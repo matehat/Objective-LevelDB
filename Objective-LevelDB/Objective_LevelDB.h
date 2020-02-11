1c2ba920d00913b3ebb6abb96ccf461d0dd1f17b
Damian Kolasinski <damian.kolasinski@appunite.com>
2020-02-11 19:36:35 +0100

    Added leveldb-library manually

--- Objective-LevelDB/Objective_LevelDB.h
+++ Objective-LevelDB/Objective_LevelDB.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char Objective_LevelDBVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Objective_LevelDB/PublicHeader.h>
 
-
+#import "LevelDB.h"
