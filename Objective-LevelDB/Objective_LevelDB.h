1d7b8dae4cd468643b2bc98906d9fd1082cf89e0
Damian Kolasinski <damian.kolasinski@appunite.com>
2020-02-11 18:26:56 +0100

    Added pbxproj

---
+++ Objective-LevelDB/Objective_LevelDB.h
@@ -0,0 +1,19 @@
+//
+//  Objective_LevelDB.h
+//  Objective-LevelDB
+//
+//  Created by Damian Kolasiński on 11/02/2020.
+//  Copyright © 2020 AppUnite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Objective_LevelDB.
+FOUNDATION_EXPORT double Objective_LevelDBVersionNumber;
+
+//! Project version string for Objective_LevelDB.
+FOUNDATION_EXPORT const unsigned char Objective_LevelDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Objective_LevelDB/PublicHeader.h>
+
+
